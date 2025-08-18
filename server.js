edf28ea05c0fb9ec05d40f305e0eb31a776da13c
jo-oseeph <realsejjo2001@gmail.com>
2025-08-18 17:00:02 +0300

    18th August 2025

--- server.js
+++ server.js
@@ -4,25 +4,33 @@ dotenv.config();
 
 // Import libraries
 import express from 'express';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authroutes.js';
 import noteRoutes from './routes/noteRoutes.js';
-
 
 connectDB();
 
 // Create express app
 const app = express();
 
+// Middleware: Enable CORS
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*', // allow frontend origin or all if not defined
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 // Middleware: Parse incoming JSON in request body
 app.use(express.json());
 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
 
 // Test route to confirm server is running
 app.get('/', (req, res) => {
-  res.send(' MoiLearn Backend is running!');
+  res.send('MoiLearn Backend is running!');
 });
 
 // Define a port from env file or fallback to 5000
@@ -30,5 +38,5 @@ const PORT = process.env.PORT || 5000;
 
 // Start the server and listen on the port
 app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
