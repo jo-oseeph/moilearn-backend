4e8a2cd6c31825ff0541434e674d83af765733bc
jo-oseeph <realsejjo2001@gmail.com>
2026-02-13 00:08:22 +0300

    13th Feb 2026

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from './routes/authRoutes.js';
 import noteRoutes from './routes/noteRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import userRoutes from "./routes/userRoutes.js";
-import User from './models/User.js';
+
 
 // Connect to database
 connectDB();
@@ -52,30 +52,7 @@ app.get('/', (req, res) => {
 });
 
 // Add this AFTER your existing routes (before app.listen)
-app.get('/api/migrate-users', async (req, res) => {
-  try {
-    const usersToUpdate = await User.find({
-      $or: [
-        { authMethods: { $exists: false } },
-        { authMethods: { $size: 0 } }
-      ]
-    });
 
-    for (const user of usersToUpdate) {
-      const authMethods = [];
-      if (user.password) authMethods.push('email');
-      if (user.googleId || user.provider === 'google') authMethods.push('google');
-      if (authMethods.length === 0) authMethods.push('email');
-      
-      user.authMethods = authMethods;
-      await user.save();
-    }
-
-    res.json({ success: true, migrated: usersToUpdate.length });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
